855b574c24e165fab42da806c360e41ec4ea3fc2
Jeremy Toussaint <jeremyt@uio.im>
2014-11-06 23:00:49 +0100

    initial commit client

---
+++ public/js/controllers.js
@@ -0,0 +1,11 @@
+/**
+ * Created by jeremyt on 10/10/14.
+ */
+
+
+nbrAppControllers.controller("MainCtrl", function ($scope, $location) {
+        console.log('--> main started');
+
+
+    }
+);
---
+++ public/js/services.js
@@ -0,0 +1,11 @@
+/**
+ * Created by jeremyt on 10/10/14.
+ */
+
+nbrAppServices.factory('NbrService', function ($http) {
+    return {
+        /*getSpaceId: function (data) {
+            return $http.post(options.api.base_url + '/createspace', data);
+        }*/
+    }
+});
