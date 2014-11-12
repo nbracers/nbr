4b68a690fc290580c0fbfa8e0def9701c0403e14
jt151077 <jt151077@gmail.com>
2014-11-12 21:56:07 +0100

    styling and code formatting

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -3,22 +3,64 @@
  */
 
 nbrAppControllers.controller("NavCtrl", function ($scope, $location, $timeout, $mdSidenav) {
-        console.log('--> nav started');
 
+        /*
+            scope variables
+         */
+        $scope.menuSelectedIndex = 0;
+        $scope.menuButtonObject = [
+            {label: 'Main', target: '#/', disabled: false},
+            {label: 'Multihero', target: '#/hero', disabled: false},
+            {label: 'President finder', target: '#/president', disabled: true},
+            {label: 'About', target: '#/about', disabled: true}
+        ];
+
+        /*
+            listener for menu change events
+         */
+        $scope.$on('MENU_CHANGED', function (event, ind) {
+            $scope.menuSelectedIndex = ind;
+            $scope.$apply();
+        });
+
+        /*
+            toggle open side-menu
+        */
         $scope.toggleLeft = function() {
             $mdSidenav('left').toggle();
+        };
+
+        $scope.close = function() {
+            $mdSidenav('left').close();
         };
 
     }
 );
 
 
-nbrAppControllers.controller("MainCtrl", function ($scope, $location, $timeout, $mdSidenav) {
+nbrAppControllers.controller("MainCtrl", function ($scope, $rootScope, $location, $timeout, $mdSidenav) {
+        /*
+            on activate, fires MENU_CHANGED with correct index
+         */
+        $rootScope.$broadcast('MENU_CHANGED', 0);
+
+        /*
+            scope variables
+         */
+        $scope.footerText = "main";
+
     }
 );
 
-nbrAppControllers.controller('LeftCtrl', function ($scope, $timeout, $mdSidenav) {
-    $scope.close = function() {
-        $mdSidenav('left').close();
-    };
-})
+nbrAppControllers.controller("HeroCtrl", function ($scope, $rootScope, $location, $timeout, $mdSidenav) {
+        /*
+            on activate, fires MENU_CHANGED with correct index
+         */
+        $rootScope.$broadcast('MENU_CHANGED', 1);
+
+        /*
+            scope variables
+         */
+        $scope.footerText = "hero";
+    }
+);
