a86c4b2a5e7fd0e8c6bed78a5e87945d599f348e
jt151077 <jt151077@gmail.com>
2014-11-12 17:24:45 +0100

    basic styling and menu effect - responsive

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -2,14 +2,18 @@
  * Created by jeremyt on 10/10/14.
  */
 
-
-nbrAppControllers.controller("MainCtrl", function ($scope, $location, $timeout, $mdSidenav) {
-        console.log('--> main started');
+nbrAppControllers.controller("NavCtrl", function ($scope, $location, $timeout, $mdSidenav) {
+        console.log('--> nav started');
 
         $scope.toggleLeft = function() {
             $mdSidenav('left').toggle();
         };
 
+    }
+);
+
+
+nbrAppControllers.controller("MainCtrl", function ($scope, $location, $timeout, $mdSidenav) {
     }
 );
 
