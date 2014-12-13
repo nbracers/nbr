f4757469196f5d0d5b0abbc697c2540e1f86a407
Dominique Bye-Ribaut <Dominique.Bye.Ribaut@gmail.com>
2014-12-13 13:53:51 +0100

    default norwegian header of race type is empty string

--- models/racetype.js
+++ models/racetype.js
@@ -4,7 +4,7 @@ var Schema = mongoose.Schema;
 // Space schema
 var RacetypeSchema = new Schema({
     label:           { type: String, unique: true },
-    header_no: 		 { type: String }
+    header_no: 		 { type: String, default: ""}
 });
 
 // Export Models
