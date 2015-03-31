874f666cab069c34afd168b02bf3685c76a2cfac
Jeremy Toussaint <jt151077@gmail.com>
2015-03-31 15:47:31 +0200

    - fix bug

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -518,7 +518,7 @@ nbrAppControllers.controller("HeroCtrl", function ($scope, $rootScope, $location
         function calculatePreviousRanking(racersArray) {
 
             //get the second last array of sums
-            var sortedLastSum = $scope.arrayOfRankedCompetitionsByRacer[$scope.arrayOfRankedCompetitionsByRacer.length-2];
+            var sortedLastSum = $scope.arrayOfRankedCompetitionsByRacer[$scope.arrayOfRankedCompetitionsByRacer.length-1];
 
             //loop through the list of racers again
             racersArray.forEach(function (racerWithResults){
